2625de6600d162ea0102bab542bedd467bfaec15
Ioanna Mitroulaki <iomitroulaki@yahoo.gr>
2017-11-12 11:58:58 +0100

    Upload script gallerie file

--- js/script_gallery.js
+++ js/script_gallery.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
 
     //Variables and Array with src of the used images
     var srcImg, indexSrcImg,
-        fotoArr = ["../img/gallery/foto_1.jpg", "../img/gallery/foto_2.jpg", "../img/gallery/foto_3.jpg", "../img/gallery/foto_4.jpg", "../img/gallery/foto_5.jpg", "../img/gallery/foto_6.jpg", "../img/gallery/foto_7.jpg", "../img/gallery/foto_8.jpg", "../img/gallery/foto_9.jpg", "../img/gallery/foto_10.jpg", "../img/gallery/foto_11.jpg", "../img/gallery/foto_12.jpg"];
+        fotoArr = ["img/gallery/foto_1.jpg", "img/gallery/foto_2.jpg", "img/gallery/foto_3.jpg", "img/gallery/foto_4.jpg", "img/gallery/foto_5.jpg", "img/gallery/foto_6.jpg", "img/gallery/foto_7.jpg", "img/gallery/foto_8.jpg", "img/gallery/foto_9.jpg", "img/gallery/foto_10.jpg", "img/gallery/foto_11.jpg", "img/gallery/foto_12.jpg"];
     
     //Prepend img in the display of gallery
-    $('.display-foto').prepend('<img src="../img/gallery/foto_1.jpg" alt="Photo of our restaurant">');
+    $('.display-foto').prepend('<img src="img/gallery/foto_1.jpg" alt="Photo of our restaurant">');
     
     //Change image when the arrows are clicked
     $('.left-arrow i').click(function () {
